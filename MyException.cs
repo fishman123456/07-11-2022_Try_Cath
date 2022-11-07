b8d7f9bb60275e1e85f0ff5921217e26a285ba47
F_D <den9152@yandex.ru>
2022-11-07 20:39:58 +0700

    Initial commit

---
+++ MyException.cs
@@ -0,0 +1,12 @@
+﻿[Serializable]
+public class MyException : Exception
+{
+	public DateTime TimeExcept { get;private set; }
+	public MyException() : base ("ошибка"){
+	TimeExcept = DateTime.Now;
+	}
+	public MyException(string message) : base(message)
+	{
+		TimeExcept = DateTime.Now;
+	}
+}    
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Создать свой класс исключения, выводящий сообщение об ошибке и текущюю дату.
+// Использовать его в теле следующей программы
+// Вводят число А и В. В случае если чило А не делится на число В без 
+// остатка, выдать сообщение об ошибке.
+Console.WriteLine("Введите число А");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число В");
+int B = int.Parse(Console.ReadLine());
+
+try
+{
+    if (A % B != 0)
+    {
+        throw new MyException("Не делится без остатка"); }
+    else Console.WriteLine("делится без остатка");
+    
+}
+catch (MyException e)
+{
+    Console.WriteLine(e.TimeExcept);
+    Console.WriteLine(e.Message);
+    //вывод стека трассировки
+    
+}
